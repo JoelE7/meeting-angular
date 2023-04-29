a766f6467e2ae128c212ef38e3980c8ac1fc8b04
Ezequiel <ezequiel.sanson@hotmail.com>
2023-04-29 10:34:25 -0300

    Agrego Login

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './pages/login/login.component';
+import { PrimengModule } from '../shared/primeng/primeng.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RegisterComponent } from './pages/register/register.component';
-
+import { AppModule } from '../app.module';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  declarations: [
-    LoginComponent,
-    RegisterComponent
-  ],
+  declarations: [LoginComponent, RegisterComponent],
   imports: [
     CommonModule,
+    PrimengModule,
+    ReactiveFormsModule,
+    FormsModule,
     AuthRoutingModule
-  ]
+  ],
+  exports: [LoginComponent, RegisterComponent],
 })
-export class AuthModule { }
+export class AuthModule {}
