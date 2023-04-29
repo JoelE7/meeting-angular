874c2c990e81af72b3fd39f21b7d11e8c9afdbba
jessica <jessica@DESKTOP-RN8N545>
2023-04-29 11:06:44 -0300

    listado de proyectos

--- src/app/components/project/project.module.ts
+++ src/app/components/project/project.module.ts
@@ -1,14 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { ProjectRoutingModule } from './project-routing.module';
-
+import { CreateProjectComponent } from './pages/create-project/create-project.component';
+import { FormProjectComponent } from './components/form-project/form-project.component';
+import { ListProjectComponent } from './pages/list-project/list-project.component';
+import { CardProjectComponent } from './components/card-project/card-project.component';
+import { PrimengModule } from 'src/app/shared/primeng/primeng.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateProjectComponent,
+    ListProjectComponent,
+    CardProjectComponent,
+    FormProjectComponent
+  ],
   imports: [
     CommonModule,
-    ProjectRoutingModule
+    ProjectRoutingModule,
+    PrimengModule,
+  ],
+  exports:[
+    CreateProjectComponent,
+    FormProjectComponent,
+    ListProjectComponent,
+    CardProjectComponent,
+
   ]
 })
 export class ProjectModule { }
