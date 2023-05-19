a8264fd2f02a3606efe460b513ab33b57e4185e2
JoelE7 <71388749+JoelE7@users.noreply.github.com>
2023-05-18 22:11:33 -0300

    arreglo de test

--- src/app/test/__mocks__/services/auth/auth.service.mock.ts
+++ src/app/test/__mocks__/services/auth/auth.service.mock.ts
@@ -3,7 +3,9 @@ import { Observable, of } from 'rxjs';
 export const mockAuthService: {
   loginUser: () => Observable<any>;
   registerUser: () => Observable<any>;
+  setLogin: ()=> {}
 } = {
   loginUser: () => of(['']),
   registerUser: () => of(['']),
+  setLogin : ()=> of({})
 };
