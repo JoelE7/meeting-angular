6bd563c526fee236fbc256bbe4ec9687f99d2bac
JoelE7 <71388749+JoelE7@users.noreply.github.com>
2023-06-13 20:53:51 -0300

    cambio de enviroment de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    apiUrl: 'https://meeting-nodejs-production.up.railway.app/api',
+    apiUrl: 'https://meeting-nodejs-production-e246.up.railway.app/api',
     frontUrl: '',
     production: true,
   };
