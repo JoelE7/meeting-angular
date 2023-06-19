07a7eb8e2c68e51cc922e9a1060370f36a04be06
JoelE7 <71388749+JoelE7@users.noreply.github.com>
2023-06-19 14:45:01 -0300

    inicio de template de pdf

--- src/app/api/services/document/templates/templates.service.ts
+++ src/app/api/services/document/templates/templates.service.ts
@@ -3,16 +3,38 @@ import { Project } from 'src/app/shared/models/project/project.class';
 import { User } from 'src/app/shared/models/user/user.class';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TemplatesService {
+  constructor() {}
 
-  constructor() { }
+  downloadCertificate(project: Project, user: User) {
+    let html = `
+    <!DOCTYPE html>
+    <html lang="es">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Certificado ${project.name}</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    </head>
+    <body>
+      
+    </body>
+    </html>
+    `
+    for (let i = 0; i <5; i++) {
+      const element = [i];
+      html += `${i}`
+      
+    }
 
-  downloadCertificate(project:Project,user:User){
-
+    html = `
+    
+    `
+    
+    ;
+    return html;
   }
-
-
-
 }
