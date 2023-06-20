0ffd11fe6d91a172d6d50857212e3b3850fe2053
JoelE7 <71388749+JoelE7@users.noreply.github.com>
2023-06-20 18:26:54 -0300

    fix

--- src/app/modules/post/components/form-post/form-post.component.ts
+++ src/app/modules/post/components/form-post/form-post.component.ts
@@ -54,45 +54,7 @@ export class FormPostComponent implements OnInit {
   }
 
   textChange(event) {
-    // let inner = document.getElementById('inner')
-    // inner.innerHTML = event.htmlValue
     this.editor = event.htmlValue;
-    console.log(event);
-    
-  //   const regex =
-  //   /<pre class="ql-syntax" spellcheck="false">([\s\S]*?)<\/pre>/g;
-  // const htmlContent = event.htmlValue;
-  // let htmlCode = '';
-
-  // let match;
-  // while ((match = regex.exec(htmlContent)) !== null) {
-  //   const codeBlock = match[1];
-  //   const div = document.createElement('div');
-  //   div.innerHTML = codeBlock;
-  //   htmlCode += div.textContent || div.innerText;
-  // }
-  // this.editor = htmlCode;
-  // console.log(htmlCode);
-
-    // if (this.modeCodeBlock) {
-    //   const regex =
-    //     /<pre class="ql-syntax" spellcheck="false">([\s\S]*?)<\/pre>/g;
-    //   const htmlContent = event.htmlValue;
-    //   let htmlCode = '';
-
-    //   let match;
-    //   while ((match = regex.exec(htmlContent)) !== null) {
-    //     const codeBlock = match[1];
-    //     const div = document.createElement('div');
-    //     div.innerHTML = codeBlock;
-    //     htmlCode += div.textContent || div.innerText;
-    //   }
-    //   this.editor = htmlCode;
-    //   console.log(htmlCode);
-      
-    // } else {
-    //   this.editor = event.textValue;
-    // }
   }
 
   getTechnologies() {
@@ -132,10 +94,13 @@ export class FormPostComponent implements OnInit {
   submitPost() {
     this.newPost.title = this.form.get('title').value;
     this.newPost.body = this.editor;
-    this.newPost.project = this.form.get('project').value?._id;
+    this.newPost.project = this.form.get('project').value?._id || "";
     this.newPost.date = new Date();
     this.newPost.author = this.user._id;
     this.newPost.technologies = this.form.get('technologies').value;
+
+    console.log(this.newPost);
+    
 
     this.emitPost.emit(this.newPost);
   }
