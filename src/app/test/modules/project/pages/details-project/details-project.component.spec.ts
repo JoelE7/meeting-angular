a4605b84faf157d761516c45077728d61a2bc338
JoelE7 <71388749+JoelE7@users.noreply.github.com>
2023-06-26 23:25:59 -0300

    fix test 15

--- src/app/test/modules/project/pages/details-project/details-project.component.spec.ts
+++ src/app/test/modules/project/pages/details-project/details-project.component.spec.ts
@@ -67,11 +67,11 @@ describe('DetailsProjectComponentConLogin', () => {
     localStorage.removeItem('user');
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('si el proyecto está vinculado a un repo de github o gitlab que se traiga las métricas : getMetricByProject()', async() => {
+  it('si el proyecto está vinculado a un repo de github o gitlab que se traiga las métricas : getMetricByProject()', async() => {
     const detailsProjectAsync = spyOn(
       mockProjectService,
       'detailsProjectAsync'
@@ -89,7 +89,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.getMetricByProject).toHaveBeenCalled();
   });
 
-  xit('si el proyecto no está vinculado a un repo de github o gitlab que no traiga las métricas : getMetricByProject()', async() => {
+  it('si el proyecto no está vinculado a un repo de github o gitlab que no traiga las métricas : getMetricByProject()', async() => {
     const detailsProjectAsync = spyOn(mockProjectService,'detailsProjectAsync')
     detailsProjectAsync.and.returnValue(
       Promise.resolve(mockProjectDetails)
@@ -102,7 +102,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.getMetricByProject).not.toHaveBeenCalled();
   });
 
-  xit('verificar si el usuario existe en el proyecto : checkUserIfExistsInProject()',async ()=>{
+  it('verificar si el usuario existe en el proyecto : checkUserIfExistsInProject()',async ()=>{
     component.currentUser = userMock
     const detailsProjectAsync = spyOn(mockProjectService,'detailsProjectAsync')
     detailsProjectAsync.and.returnValue(
@@ -115,7 +115,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(component.userExistProject).toBeTruthy()
   })
   
-  xit('verificar si el usuario envío solicitud al proyecto : checkUserIfSendRequest()',async ()=>{
+  it('verificar si el usuario envío solicitud al proyecto : checkUserIfSendRequest()',async ()=>{
     component.currentUser = userMock
     const detailsProjectAsync = spyOn(mockProjectService,'detailsProjectAsync')
     detailsProjectAsync.and.returnValue(
@@ -128,7 +128,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(component.userExistSendRequest).toBeTruthy()
   })
 
-  xit('que se pueda linkear el proyecto a un repositorio de github/gitlab : linkProjectToRepository()',async ()=>{
+  it('que se pueda linkear el proyecto a un repositorio de github/gitlab : linkProjectToRepository()',async ()=>{
     const updateProject = spyOn(mockProjectService,'updateProject')
     updateProject.and.returnValue(of<any>({}));
     component.linkRepositoryProject = "https:github.com"
@@ -137,14 +137,14 @@ describe('DetailsProjectComponentConLogin', () => {
   })
 
 
-  xit('que falle en caso de que no haya puesto una url de proyecto a un repositorio de github/gitlab : linkProjectToRepository()',async ()=>{
+  it('que falle en caso de que no haya puesto una url de proyecto a un repositorio de github/gitlab : linkProjectToRepository()',async ()=>{
     const updateProject = spyOn(mockProjectService,'updateProject')
     updateProject.and.returnValue(of<any>({}));
     await component.linkProjectToRepository();
     expect(mockProjectService.updateProject).not.toHaveBeenCalled();
   })
 
-  xit('que el usuario logueado se pueda unir a un proyecto : joinProject()',()=>{
+  it('que el usuario logueado se pueda unir a un proyecto : joinProject()',()=>{
     component.currentUser = userMock2
     const joinProject = spyOn(mockProjectService,'joinProject')
     joinProject.and.returnValue(of<any>({}));
@@ -152,7 +152,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.joinProject).toHaveBeenCalled();
   })
 
-  xit('que se pueda cancelar una solicitud para unirse a un proyecto : cancelRequestToJoinTheProject()',()=>{
+  it('que se pueda cancelar una solicitud para unirse a un proyecto : cancelRequestToJoinTheProject()',()=>{
     component.currentUser = userMock2
     const cancelRequestToJoinTheProject = spyOn(mockProjectService,'cancelRequestToJoinTheProject')
     cancelRequestToJoinTheProject.and.returnValue(of<any>({}));
@@ -160,7 +160,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.cancelRequestToJoinTheProject).toHaveBeenCalled();
   })
 
-  xit('que se pueda enviar una solicitud para unirse a un proyecto : sendRequestToJoinTheProject()',()=>{
+  it('que se pueda enviar una solicitud para unirse a un proyecto : sendRequestToJoinTheProject()',()=>{
     component.currentUser = userMock2
     const sendRequestToJoinTheProject = spyOn(mockProjectService,'sendRequestToJoinTheProject')
     sendRequestToJoinTheProject.and.returnValue(of<any>({}));
@@ -168,7 +168,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.sendRequestToJoinTheProject).toHaveBeenCalled();
   })
 
-  xit('que el usuario pueda abandonar un proyecto : leaveProject()',()=>{
+  it('que el usuario pueda abandonar un proyecto : leaveProject()',()=>{
     component.currentUser = userMock2
     const leaveProject = spyOn(mockProjectService,'leaveProject')
     leaveProject.and.returnValue(of<any>({}));
@@ -176,7 +176,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.leaveProject).toHaveBeenCalled();
   })
 
-  xit('que el usuario pueda aceptar o rechazar una solicitud',()=>{
+  it('que el usuario pueda aceptar o rechazar una solicitud',()=>{
     component.currentUser = userMock2
     const userRequestResponsesLeader = spyOn(mockProjectService,'userRequestResponsesLeader')
     userRequestResponsesLeader.and.returnValue(of<any>({}));
@@ -184,7 +184,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.userRequestResponsesLeader).toHaveBeenCalled();
   })
 
-  xit('que el usuario pueda sugerir un proyecto : sendMailInvitation()',()=>{
+  it('que el usuario pueda sugerir un proyecto : sendMailInvitation()',()=>{
     component.currentUser = userMock2
     const sendMailInvitation = spyOn(mockProjectService,'sendMailInvitation')
     sendMailInvitation.and.returnValue(of<any>({}));
@@ -193,7 +193,7 @@ describe('DetailsProjectComponentConLogin', () => {
   })
 
 
-  xit('que se pueda finalizar un proyecto : finalizeProject()',()=>{
+  it('que se pueda finalizar un proyecto : finalizeProject()',()=>{
     component.currentUser = userMock2
     const finalizeProject = spyOn(mockProjectService,'finalizeProject')
     finalizeProject.and.returnValue(of<any>({}));
@@ -236,11 +236,11 @@ describe('DetailsProjectComponentSinLogin', () => {
     fixture.detectChanges();
   });
 
-  xit('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  xit('que al iniciar el componente se llame a : detailsProject', () => {
+  it('que al iniciar el componente se llame a : detailsProject', () => {
     const detailsProjectAsync = spyOn(
       mockProjectService,
       'detailsProjectAsync'
@@ -255,7 +255,7 @@ describe('DetailsProjectComponentSinLogin', () => {
     expect(component.searchProject._id).toEqual('1');
   });
 
-  xit('si el usuario no está logueado que no se pueda unir a un proyecto',()=>{
+  it('si el usuario no está logueado que no se pueda unir a un proyecto',()=>{
     const joinProject = spyOn(mockProjectService,'joinProject')
     joinProject.and.returnValue(of<any>({}));
     component.joinAProject();
