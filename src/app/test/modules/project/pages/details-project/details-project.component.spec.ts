c399eb4133093c90585d52e0623713b05c61e326
JoelE7 <71388749+JoelE7@users.noreply.github.com>
2023-06-27 00:16:49 -0300

    fix test 27

--- src/app/test/modules/project/pages/details-project/details-project.component.spec.ts
+++ src/app/test/modules/project/pages/details-project/details-project.component.spec.ts
@@ -166,7 +166,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.sendRequestToJoinTheProject).toHaveBeenCalled();
   })
 
-  xit('que el usuario pueda abandonar un proyecto : leaveProject()',()=>{
+  it('que el usuario pueda abandonar un proyecto : leaveProject()',()=>{
     component.currentUser = userMock2
     const leaveProject = spyOn(mockProjectService,'leaveProject')
     leaveProject.and.returnValue(of<any>({}));
@@ -174,7 +174,7 @@ describe('DetailsProjectComponentConLogin', () => {
     expect(mockProjectService.leaveProject).toHaveBeenCalled();
   })
 
-  xit('que el usuario pueda aceptar o rechazar una solicitud',()=>{
+  it('que el usuario pueda aceptar o rechazar una solicitud',()=>{
     component.currentUser = userMock2
     const userRequestResponsesLeader = spyOn(mockProjectService,'userRequestResponsesLeader')
     userRequestResponsesLeader.and.returnValue(of<any>({}));
